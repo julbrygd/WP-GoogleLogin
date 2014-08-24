5c2c7d0a35a4173452dc372b797be196e700e6e8
Stephan Conrad <stephan.conrad@gmail.com>
2014-08-24 18:27:18 +0200

    Login possible

--- SettingsMenu.php
+++ SettingsMenu.php
@@ -9,6 +9,7 @@ class GoogleLogin_Settings {
 
     private static $INSTANCE = null;
     private static $NONCE_NAME = "googleLogin_settings";
+    private static $SETTINGS_KEY = "gl_settings_array";
 
     public static function getInstance() {
         if (self::$INSTANCE == null) {
@@ -19,32 +20,73 @@ class GoogleLogin_Settings {
 
     private function __construct() {
         add_action('admin_menu', array(&$this, "addOptionPage"));
+        add_action('admin_post_gl_settings_page_save', array(&$this, "saveSettings"));
     }
 
     public function addOptionPage() {
         add_options_page("Google Login", "Google Login", "create_users", "gl_settings_page", array(&$this, "showLoginPage"));
     }
 
-    public function showLoginPage() {
-        if (isset($_REQUEST["action"]) && $_REQUEST["action"] == "update") {
-            print_r($_REQUEST);
+    public function saveSettings() {
+        if (isset($_REQUEST["action"]) && $_REQUEST["action"] == "gl_settings_page_save" && wp_verify_nonce($_REQUEST["_wpnonce"], self::$NONCE_NAME)) {
+            if (!current_user_can('manage_options')) {
+                wp_die('You are not allowed to be on this page.');
+            }
+            // Check that nonce field
+            check_admin_referer(self::$NONCE_NAME);
+
+            $options = get_option(self::$SETTINGS_KEY);
+
+            if (isset($_POST['client_id'])) {
+                $options['client_id'] = sanitize_text_field($_POST['client_id']);
+            }
+            if (isset($_POST['client_secret'])) {
+                $options['client_secret'] = sanitize_text_field($_POST['client_secret']);
+            }
+            if (isset($_POST['email_address'])) {
+                $options['email_address'] = sanitize_text_field($_POST['email_address']);
+            }
+
+            update_option(self::$SETTINGS_KEY, $options);
+
+            wp_redirect(admin_url('options-general.php?page=gl_settings_page'));
+            exit;
         }
+    }
+
+    public function showLoginPage() {
+        $options = get_option(self::$SETTINGS_KEY);
         ?>
         <div class="wrap">
             <h2>Einstellungen › Google Login</h2>
 
-            <form action="options.php" method="post">
-                <input type="hidden" name="option_page" value="gl_settings_page">
-                <input type="hidden" name="action" value="update">
-                <input type="hidden" id="_wpnonce" name="_wpnonce" value="<?php echo wp_create_nonce(self::$NONCE_NAME); ?>">
+            <form action="<?php echo admin_url('admin-post.php'); ?>" method="post">
+                <input type="hidden" name="action" value="gl_settings_page_save">
+                <?php wp_nonce_field( self::$NONCE_NAME ); ?>
                 <input type="hidden" name="_wp_http_referer" value="/wordpress/wp-admin/options-general.php?page=gl_settings_page">
                 <h3 class="title">Google API</h3>
                 <p>Hier sind die Einstellungen f&uuml;r die Google API</p>
                 <table class="form-table">
-                    <tbody><tr>
+                    <tbody>
+                        <tr>
                             <th scope="row">Client ID</th>
                             <td>
-                                <input name="client_id" type="text" id="client_id" class="large-text">
+                                <input name="client_id" type="text" id="client_id" class="large-text" value="<?php echo $options['client_id'];?>">
+
+                            </td>
+                        </tr>
+                         <tr>
+                            <th scope="row">CLIENT SECRET</th>
+                            <td>
+                                <input name="client_secret" type="text" id="client_id" class="large-text" value="<?php echo $options['client_secret'];?>">
+
+                            </td>
+                        </tr>
+                        </tr>
+                         <tr>
+                            <th scope="row">EMAIL ADDRESS</th>
+                            <td>
+                                <input name="email_address" type="text" id="client_id" class="large-text" value="<?php echo $options['email_address'];?>">
 
                             </td>
                         </tr>
@@ -54,6 +96,14 @@ class GoogleLogin_Settings {
             </form>
         </div>
         <?php
+    }
+    
+    public function getOptions() {
+        return get_option(self::$SETTINGS_KEY);
+    }
+    
+    public function getOption($name) {
+        return get_option(self::$SETTINGS_KEY)[$name];
     }
 
 }
--- googleLogin.php
+++ googleLogin.php
@@ -11,6 +11,8 @@
 define("GOOGLE_LOGIN_PLUGIN_DIR", WP_PLUGIN_DIR . "/googleLogin");
 define("GOOGLE_LOGIN_PLUGIN_URL", plugins_url() . "/googleLogin");
 
+set_include_path(get_include_path() . PATH_SEPARATOR . GOOGLE_LOGIN_PLUGIN_DIR . '/vendor/google/apiclient/src');
+
 require_once GOOGLE_LOGIN_PLUGIN_DIR . '/vendor/autoload.php';
 require_once GOOGLE_LOGIN_PLUGIN_DIR . '/SettingsMenu.php';
 
@@ -23,6 +25,7 @@ class GoogleLogin {
         add_action('login_form', array(&$this, 'loginForm'));
         add_action('login_enqueue_scripts', array(&$this, 'enqueueStyle'), 10);
         add_action('login_enqueue_scripts', array(&$this, 'enqueueScript'), 1);
+        $this->adminAjax();
         $this->settingsObject = GoogleLogin_Settings::getInstance();
     }
 
@@ -31,41 +34,106 @@ class GoogleLogin {
     }
 
     public function enqueueScript() {
-        
+        wp_enqueue_script("googleLoginJs", GOOGLE_LOGIN_PLUGIN_URL . "/js/loginScript.js", array('jquery'));
     }
 
     public function loginForm() {
         ?>
+        <p>
         <lable>Einloggen mit:</lable><br />
 
         <span id="signinButton">
             <span
                 class="g-signin"
                 data-callback="signinCallback"
-                data-clientid="824683902314-uar96f562e4df03n8l5ss8uh197iep9o.apps.googleusercontent.com"
-                data-cookiepolicy="single_host_origin"
-                data-requestvisibleactions="http://schemas.google.com/AddActivity"
-                data-scope="https://www.googleapis.com/auth/plus.login">
+                data-clientid="<?php echo $this->settingsObject->getOption("client_id"); ?>"
+                data-redirecturi="postmessage"
+                data-accesstype="offline"
+                data-cookiepolicy="none"
+                data-approvalprompt="force"
+                data-scope="https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar">
             </span>
         </span>
-
+        </p>
         <?php
     }
 
     public function loginHead() {
         ?>
         <script type="text/javascript">
+            var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
             (function() {
                 var po = document.createElement('script');
                 po.type = 'text/javascript';
                 po.async = true;
-                po.src = 'https://apis.google.com/js/client:plusone.js';
+                po.src = 'https://plus.google.com/js/client:plusone.js?onload=start';
                 var s = document.getElementsByTagName('script')[0];
                 s.parentNode.insertBefore(po, s);
             })();
         </script>
 
         <?php
+    }
+
+    public function adminAjax() {
+        add_action('wp_ajax_gl_login', array(&$this, "loginUser"));
+        add_action('wp_ajax_nopriv_gl_login', array(&$this, "loginUser"));
+    }
+
+    public function loginUser() {
+        if (isset($_POST["auth"])) {
+            $authPost = $_POST["auth"];
+            $googleSettings = $this->settingsObject->getOptions();
+            $client = new Google_Client();
+            $client->setClientId($googleSettings["client_id"]);
+            $client->setClientSecret($googleSettings["client_secret"]);
+            $client->setRedirectUri("postmessage");
+            $client->authenticate($authPost);
+            if ($client->isAccessTokenExpired()) {
+                $NewAccessToken = json_decode($client->authenticate($authPost));
+                $client->refreshToken($NewAccessToken->refresh_token);
+            }
+            $plus = new Google_Service_Plus($client);
+            $me = $plus->people->get('me');
+            $mails = $me->getEmails();
+            $mail = "";
+            if (count($mails) == 1) {
+                $mail = $mails[0]["value"];
+            } else {
+                foreach ($mails as $v) {
+                    if ($v["type"] == "account") {
+                        $mail = ["value"];
+                    }
+                }
+            }
+            $username = $me->getNickname();
+            if ($username == "") {
+                $username = $mail;
+            }
+            $user_id = username_exists($username);
+            if (!$user_id and email_exists($mail) == false) {
+                $random_password = wp_generate_password($length = 12, $include_standard_special_chars = false);
+                $user_id = wp_create_user($username, $random_password, $mail);
+                $data = get_userdata($user_id);
+                $data->display_name = $me->getDisplayName();
+                $data->first_name = $me->getName()->getGivenName();
+                $data->last_name = $me->getName()->getFamilyName();
+                wp_update_user($data);
+                $data = get_userdata($user_id);
+                file_put_contents(GOOGLE_LOGIN_PLUGIN_DIR . "/log.txt", "Userdata: " . print_r($data, true));
+                file_put_contents(GOOGLE_LOGIN_PLUGIN_DIR . "/log.txt", "Plusdata: " . print_r($me, true), FILE_APPEND);
+            }
+
+            // log in automatically
+            if (!is_user_logged_in()) {
+                $user = get_userdatabylogin($username);
+                $user_id = $user->ID;
+                wp_set_current_user($user_id, $user_login);
+                wp_set_auth_cookie($user_id);
+                do_action('wp_login', $user_login);
+            }
+        }
+        exit();
     }
 
 }
